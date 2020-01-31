6abf5ef68c7fb8c4607b2f5349986dcedd964003
josh <jhell85@gmail.com>
2020-01-31 11:04:54 -0800

    updated radio button names

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,25 @@
 
 // -------UI-------
+var Order = new Order();
+
+$(".pizza-size").click(function(){
+  console.log("radio selected")
+})
+
 $(document).ready(function(){
+
+
 
 });
 
 
 //---------Back end-----------
+
+function Order() {
+  this.Pizzas = [];
+  this.currentId = 0;
+}
+
 function Pizza(pizzaSize, pizzaPrice) {
   this.size = pizzaSize;
   this.toppings = [];
