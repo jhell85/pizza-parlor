bac23adeeca44d5bd33ffedd646e4a56a5bd0caf
josh <jhell85@gmail.com>
2020-02-09 11:16:12 -0800

    fixed issues with Pizza object

--- js/scripts.js
+++ js/scripts.js
@@ -3,56 +3,59 @@
 function Order() {
   this.pizzas = [];
   this.currentId = 0;
-  this.price = 0
+  this.price = 0;
 }
 
 Order.prototype.addPizza = function(pizza) {
-  pizza.price = getPrice(pizza)
-  pizza.id = this.assignId()
-  this.pizzas.push(pizza)
-  this.price += pizza.price
+  pizza.price = pizza.getPrice();
+  pizza.id = this.assignId();
+  this.pizzas.push(pizza);
+  this.price += pizza.price;
 }
+
 Order.prototype.addOrderTotal = function(pizza){
-  this.price += pizza.price
+  this.price += pizza.price;
 }
 
 Order.prototype.assignId = function () {
   this.currentId ++;
-  return this.currentId
+  return this.currentId;
 }
 //------------Pizza Logic-----------------
-function Pizza(pizzaSize,toppings,pizzaPrice) {
+class Pizza {
+  constructor(pizzaSize,toppings,pizzaPrice) {
   this.size = pizzaSize;
   this.toppings = toppings;
   this.price = pizzaPrice;
-}
-function getPrice(pizza){
-  price = 0
-  if(pizza.size === "Small"){
-    price += 10
-  }else if(pizza.size === "Medium"){
-    price += 15
-  }else{
-    price += 18
   }
-  pizza.toppings.forEach(topping => {
-    if (topping === "Pepperoni" || topping === "Sausage"){
-      price += 2
-    }else if(topping === "Pineapple" || topping === "Mushrooms"){
-      price += .75
-    }
-  });
-  return price
-}
 
+  getPrice(){
+    this.price = 0;
+    if(this.size === "Small"){
+      this.price += 10;
+    }else if(this.size === "Medium"){
+      this.price += 15;
+    }else{
+      this.price += 18;
+    }
+    this.toppings.forEach(topping => {
+      if (topping === "Pepperoni" || topping === "Sausage"){
+        this.price += 2;
+      }else if(topping === "Pineapple" || topping === "Mushrooms"){
+        this.price += .75;
+      }
+    });
+    return this.price
+  }
+}
 // -------UI-------
 var order = new Order();
-var toppings = [];
+var toppings = []; 
 
 function displayPizzaToppings(toppings) {
   var toppingsList = "";
   toppings.forEach(topping => {
-    toppingsList += `<span>${topping}</span><br>`
+    toppingsList += `<span>${topping}</span><br>`;
   })
   return toppingsList
 }
@@ -61,43 +64,61 @@ function displayOrderDetails(OrderToDisplay) {
   var PizzaList = $("#pizza-table");
   var htmlForPizzaDetails = "";
   OrderToDisplay.pizzas.forEach(pizza => {
-    htmlForPizzaDetails += `<tr class="table-secondary"> <td scope="row">Pizza</td> <td>${pizza.size}</td> <td>${displayPizzaToppings(pizza.toppings)}</ul></td> <td>${pizza.price}</td>`
+    htmlForPizzaDetails += `<tr class="table-secondary"> <td scope="row">Pizza</td> <td>${pizza.size}</td> <td>${displayPizzaToppings(pizza.toppings)}</ul></td> <td>${pizza.price}</td>`;
   })
   htmlForPizzaDetails += `<tr class="table-success"> <th scope="row"><button id="button-order" class="btn btn-primary">submit Order</button><th/> <th> Total:</th> <th>${OrderToDisplay.price}</th>`
-  PizzaList.html(htmlForPizzaDetails)
+  PizzaList.html(htmlForPizzaDetails);
 }
 
-function attachContactListeners() { 
+
+
+function attachContactListeners() {
   $("input.toppings").on("change", function(){
     var topping = $(this);
     if (topping.is(":checked")) {
       toppings.push(topping.val());
+      return toppings
     }
-    else{
+    else {
       toppings = toppings.filter(i => i != topping.val());
+      return toppings
     }
   });
 
   $("#order").on("click", "#button-order", function(){
+    alert("thanks for your order we will get that to you ASAP");
+    location.reload(true);
+  });
 
-    alert("thanks for your order we will get that to you ASAP")
-  })
+  return toppings
 }
+
 function clearInputs(){
   $('input[type=checkbox]').each(function() { 
           this.checked = false; 
   }); 
   $('input[type=radio]').each(function() { 
     this.checked = false; 
-}); 
+  }); 
+}
+
+function checkSize(){
+  let smallChecked = $('#small').is(':checked');
+  let mediumChecked = $('#medium').is(':checked');
+  let largeChecked = $('#large').is(':checked');
+  if (smallChecked === false && mediumChecked === false && largeChecked === false){ 
+    alert("you must choose a size for you pie");
+    return false
+  } 
 }
 
 $(document).ready(function(){
   attachContactListeners();
   $("#form-pizza").submit(function(event) {
     event.preventDefault();
+    if (checkSize() === false){return false};
     var size = $(".pizza-size:checked").val();
-    var newPizza = new Pizza(size, toppings);
+    var newPizza = new Pizza(size, toppings, 0);
     order.addPizza(newPizza);
     displayOrderDetails(order);
     clearInputs();
@@ -105,4 +126,3 @@ $(document).ready(function(){
     console.log(`Order: ${order} toppings: ${toppings}`);
   })
 });
-
